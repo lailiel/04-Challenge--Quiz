91222bfc2f11284ab165dd401603e7dac5cede58
Abi L <124858748+lailiel@users.noreply.github.com>
2023-03-26 19:46:37 -0500

    connected start button to timer and got timer working

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,44 @@
+// var question = document.querySelector("#quesion")
+// var answers = document.querySelector("#answers")
+var timerElement= document.querySelector(".timer-count")
 
-var count = 0;
-var score = 0;
-var question = document.querySelector("#quesion")
-var answers = document.querySelector("#answers")
-//  Select increment and decrement button elements
-var start = document.querySelector("#start-button");
-var countEl = document.querySelector("#count");
+var start = document.querySelector(".start-button");
+// var countEl = document.querySelector(".count");
 
-// Updates count on page
-function setCounterText() {
-  countEl.textContent = count;
+
+// var score = 0;
+var timer; 
+var timerCount = 30;
+
+// add var arrays here for questions and answers
+
+// function init() {
+//   getHighscore();
+// }
+
+
+function startGame() {
+  timerCount = 30;
+  start.disabled = true;
+  // poseQuestion()
+  startTimer()
 }
+
+
+
+function startTimer() {
+  timer = setInterval(function() {
+    timerCount--;
+    timerElement.textContent = timerCount;
+    if (timerCount === 0) {
+      clearInterval(timer);
+      // gameOver();
+    }
+  }, 1000);
+}
+
+
+// -----------------------------------------------------------------------------------------
+
 // Attach event listener to increment button element
-start.addEventListener("click", function() {
-//   count++;
-//   setCounterText();
-});
-
-
-
-
-
-function displayMessage
-// update this with "game over"
-
-function countdown() {
-    var timeLeft = 5;
-  
-    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-    var timeInterval = setInterval(function () {
-      // As long as the `timeLeft` is greater than 1
-      if (timeLeft > 1) {
-        // Set the `textContent` of `timerEl` to show the remaining seconds
-        timerEl.textContent = timeLeft
-        // Decrement `timeLeft` by 1
-        timeLeft--;
-      } else {
-        // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-        timerEl.textContent = '';
-        // Use `clearInterval()` to stop the timer
-        clearInterval(timeInterval);
-        // Call the `displayMessage()` function
-        displayMessage();
-      }
-    }, 1000);
-  }
+start.addEventListener("click", startGame) 
