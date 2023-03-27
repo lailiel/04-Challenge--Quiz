ec51949e04070a6239f5a05ee635ecf76371f8a5
Abi L <124858748+lailiel@users.noreply.github.com>
2023-03-27 13:43:03 -0500

    added array of question/answer objects, and connected them to function that will display them on the page

--- Assets/script.js
+++ Assets/script.js
@@ -1,10 +1,10 @@
-var question = document.querySelector(".question")
+var question = document.querySelector("#question")
 var answers = document.querySelector(".answers")
 
 var answer1 = document.querySelector("#answer1")
-var answer1 = document.querySelector("#answer2")
-var answer1 = document.querySelector("#answer3")
-var answer1 = document.querySelector("#answer4")
+var answer2 = document.querySelector("#answer2")
+var answer3 = document.querySelector("#answer3")
+var answer4 = document.querySelector("#answer4")
 
 var timerElement= document.querySelector(".timer-count")
 var introEl = document.querySelector("#intro")
@@ -13,7 +13,24 @@ var start = document.querySelector(".start-button");
 
 
 // add var arrays here for questions and answers
-var questionOptions
+var questionArray = [
+  {
+    questionOp: "Question 1-answer 2 is correct",
+    options: ["answer 1", "answer 2", "answer 3", "answer 4"],
+    answerCor: "answer 2",
+  },
+  {
+    questionOp: "Question 2-answer 8 is correct",
+    options: ["answer 5", "answer 6", "answer 7", "answer 8"],
+    answerCor: "answer 8",
+  },
+  {
+    questionOp: "Question 3-answer 9 is correct",
+    options: ["answer 9", "answer 10", "answer 11", "answer 12"],
+    answerCor: "answer 8",
+  },
+]
+
 
 var score = 0;
 var timer; 
@@ -27,7 +44,7 @@ var timerCount;
 
 
 function startGame() {
-  timerCount = 3;
+  timerCount = 30;
   start.disabled = true;
   startTimer()
   poseQuestion()
@@ -55,11 +72,12 @@ function startTimer() {
 function poseQuestion(){
   introEl.setAttribute("style", "visibility:hidden;");
   answers.setAttribute("style", "visibility:visible;");
-  question.textContent = questionOptions
-  answer1.textContent = answerOptions[x]
-  answer2.textContent = answerOptions[x]
-  answer3.textContent = answerOptions[x]
-  answer4.textContent = answerOptions[x]
+  // for (let i = 0; i < quesionArray.length; i++)
+  question.textContent = questionArray[0].questionOp
+  answer1.textContent = questionArray[0].options[0]
+  answer2.textContent = questionArray[0].options[1]
+  answer3.textContent = questionArray[0].options[2]
+  answer4.textContent = questionArray[0].options[3]
 }
 
 // -----------------------------------------------------------------------------------------
